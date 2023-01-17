254d30995c2cf777c57d332504a7f68b82cc7e4e
Said-laasri <saidlaasri3@gmail.com>
2023-01-17 15:13:10 +0100

    Create npx creat-app

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import React from 'react';
-import axios from 'axios';
 
 function App() {
-
-  const [data, setData] = React.useState({ value: [] });
-
-  React.useEffect(() => {
-    axios
-      .get('https://bing-news-search1.p.rapidapi.com/news', {
-        params: {safeSearch: 'Off', textFormat: 'Raw'},
-        headers: {
-          'X-BingApis-SDK': 'true',
-          'X-RapidAPI-Key': 'c0d42e0503msh6343d0903d83f48p14d1c4jsnc69016701bff',
-          'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
-        }
-      })
-      .then((response) => {
-        setData({ value: response.data.value });
-
-      });
-  }, []);
-
-
-
-
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <h1>News</h1>
-        
-        <button  >Get News</button>
-
-
-        <p>
-          { data.value.map((news) => {
-            console.log(news.image);
-            return (
-              <div>
-                <h3>{news.name}</h3>
-                <p>{news.description}</p>
-                <img src={news.image} alt={news.name} style={
-                  {width: '100px', height: '100px'}
-                } />
-              </div>
-            );
-          }, this) }
-
-
-
-        </p>
-
+    <div className="App">
+      <header className="App-header">
+        <h1>Let's get started</h1>
       </header>
     </div>
   );
