292ffa75d5f00fe09069eeeddb7d28ee58e3ccb3
Said-laasri <saidlaasri3@gmail.com>
2023-01-21 15:19:40 +0100

    Add Edit button to show and hide form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,26 +1,45 @@
-import ExpenseForm from './ExpenseForm';
-import { v4 as uuidv4 } from 'uuid';
-import Card from '../UI/Card';
-import './NewExpense.css';
-
+import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Card from "../UI/Card";
+import "./NewExpense.css";
 
 const NewExpanse = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: uuidv4()
-        };
-        props.onAddNewExpanse(expenseData);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: uuidv4(),
     };
+    props.onAddNewExpanse(expenseData);
+    setIsEditing(false);
+  };
 
+  const displayFormHandler = () => {
+    setIsEditing(true);
+  };
 
+  const hideFormHandler = () => {
+    setIsEditing(false);
+  };
 
-    return (
-        <Card className="new-expense">
-            <ExpenseForm onSaveExpanse={saveExpenseDataHandler} />
-        </Card>
-    );
-
+  return (
+    <Card className="new-expense">
+      {!isEditing && (
+        <button className="edit" onClick={displayFormHandler}>
+          Add New Expense
+        </button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          style={{ display: "none" }}
+          onSaveExpanse={saveExpenseDataHandler}
+          onCancel={hideFormHandler}
+        />
+      )}
+    </Card>
+  );
 };
 
 export default NewExpanse;
